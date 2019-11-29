53def4ea7e4746360687d0bdbcd6c304c2d9d735
NemaLove <nema6dede@gmail.com>
2019-11-29 13:06:23 +0100

    commented test file

--- src/test/java/connectclasses/ConsumerThreadTest.java
+++ src/test/java/connectclasses/ConsumerThreadTest.java
@@ -29,23 +29,24 @@ MySQLdb database = new MySQLdb(databaseConfig);
        List<Consumer> consumers = new ArrayList<>();
        for (int i = 0; i < Integer.parseInt(consumerProps.getProperty("number.of.consumers")); i++) {
            consumers.add(new ConsumerThread(consumerProps,database,1));
+           consumers.add(new ConsumerThread(consumerProps,database,2));
        }
 
        consumerGroup.assignListConsumers(consumers);
        consumerGroup.run();
    }
 
-    @Test
-    public void TestConsumer2() throws IOException {
-        consumerProps.load(new FileInputStream(consumerConfig));
-        ConsumerGroup consumerGroup = new ConsumerGroup(consumerProps, database);
-        List<Consumer> consumers = new ArrayList<>();
-        for (int i = 0; i < Integer.parseInt(consumerProps.getProperty("number.of.consumers")); i++) {
-            consumers.add(new ConsumerThread(consumerProps,database,2));
-        }
-
-        consumerGroup.assignListConsumers(consumers);
-        consumerGroup.run();
-    }
+//    @Test
+//    public void TestConsumer2() throws IOException {
+//        consumerProps.load(new FileInputStream(consumerConfig));
+//        ConsumerGroup consumerGroup = new ConsumerGroup(consumerProps, database);
+//        List<Consumer> consumers = new ArrayList<>();
+//        for (int i = 0; i < Integer.parseInt(consumerProps.getProperty("number.of.consumers")); i++) {
+//            consumers.add(new ConsumerThread(consumerProps,database,2));
+//        }
+//
+//        consumerGroup.assignListConsumers(consumers);
+//        consumerGroup.run();
+//    }
 
 }
--- src/test/java/connectclasses/ProducerThreadTest.java
+++ src/test/java/connectclasses/ProducerThreadTest.java
@@ -15,19 +15,19 @@ public class ProducerThreadTest {
 //    String configFile = "../src/main/resources/Producer.properties";
     Properties properties = new Properties();
 
-    String host = "http://localhost:8069";
+    String host = "http://localhost:8069"; //user's odoo configurations
     String db ="demodb";
     String user = "demodb@demo.com";
     String password = "123456";
-    Integer count = 1;
-    String comName = "NemaLove";
+    Integer count = 1; // assuming that each user will have an Id which will define the partition which the user's data will be on
+    String comName = "NemaLove"; // company name which will be used to name sales table
 
-    String host2 = "http://192.168.1.15:8069";
+    String host2 = "http://192.168.1.15:8069"; //user's odoo configurations
     String db2 ="demodb";
     String user2 = "demodb@demo.com";
     String password2 = "123456";
-    Integer count2 = 2;
-    String comName2 = "GLOXON";
+    Integer count2 = 2; // assuming that each user will have an Id which will define the partition which the user's data will be on
+    String comName2 = "GLOXON"; // company name which will be used to name sales table
     ConfigClass configClass;
 
     @Test
@@ -47,24 +47,25 @@ public class ProducerThreadTest {
 
         for(int i = 0; i < Integer.parseInt(properties.getProperty("number.of.producer")); i++){
             producers.add(new ProducerThread(properties,user,host,db,password,count,comName));
-        }
-        producerGroup.assignListProducer(producers);
-        producerGroup.run();
-    }
-
-    @Test
-    public void testClass2(){
-        properties.put("bootstrap.servers","127.0.0.1:9092");
-        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("topic","odooGroup");
-        properties.put("number.of.producer","3");
-        ProducerGroup producerGroup = new ProducerGroup(properties,configClass);
-        List<Producer> producers = new ArrayList<>();
-        for(int i = 0; i < Integer.parseInt(properties.getProperty("number.of.producer")); i++) {
             producers.add(new ProducerThread(properties, user2, host2, db2, password2, count2,comName2));
         }
         producerGroup.assignListProducer(producers);
         producerGroup.run();
     }
+
+//    @Test
+//    public void testClass2(){
+//        properties.put("bootstrap.servers","127.0.0.1:9092");
+//        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+//        properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+//        properties.put("topic","odooGroup");
+//        properties.put("number.of.producer","3");
+//        ProducerGroup producerGroup = new ProducerGroup(properties,configClass);
+//        List<Producer> producers = new ArrayList<>();
+//        for(int i = 0; i < Integer.parseInt(properties.getProperty("number.of.producer")); i++) {
+//            producers.add(new ProducerThread(properties, user2, host2, db2, password2, count2,comName2));
+//        }
+//        producerGroup.assignListProducer(producers);
+//        producerGroup.run();
+//    }
 }
